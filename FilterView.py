766653f4cf48a2a313c6cc238ca3c2b5db096bd5
fire-eggs <kbroutley@gmail.com>
2023-12-04 13:20:12 -0500

    Add 'smart' focus for Text. Track selected tags, pass to callback on exec. Needs cleanup/refactor.

--- FilterView.py
+++ FilterView.py
@@ -1,3 +1,5 @@
+# TODO first clause could be NOT!
+
 from tkinter import *
 from tkinter.scrolledtext import *
 from CreateToolTip import *
@@ -6,7 +8,7 @@ class FilterView(Toplevel):
     
     masterTagList = set()
     
-    def __init__(self, alltags):
+    def __init__(self, alltags, callbackfunction):
         Toplevel.__init__(self)
         self.masterTagList = alltags
         self.geometry("500x500")
@@ -56,18 +58,36 @@ class FilterView(Toplevel):
         self.update()
         self.tag1.focus() # start focus at first entry
 
-    def clearItem(self, target):
+        self.clauses = ['','','','']
+        self.widgets = [self.tag1, self.tag2, self.tag3, self.tag4]
+        self.callback = callbackfunction
+        
+    def clearItem(self, index):
+        target = self.widgets[index]
         target.configure(state="normal")
         target.delete('1.0', END)
         target.configure(state="disabled")
-            
+        self.clauses[index] = ''
+        
     def clickReset(self):
-        self.clearItem(self.tag1)
-        self.clearItem(self.tag2)
-        self.clearItem(self.tag3)
-        self.clearItem(self.tag4)
+        self.clearItem(0)
+        self.clearItem(1)
+        self.clearItem(2)
+        self.clearItem(3)
+#        self.clearItem(self.tag1)
+#        self.clearItem(self.tag2)
+#        self.clearItem(self.tag3)
+#        self.clearItem(self.tag4)
+        self.tag1.focus() # start focus at first entry
+        # TODO reset boolean widgets
     
     def clickWrite(self):
+        if self.callback is not None:
+            self.callback(self.clauses)
+#        for i in range(4):
+#            val = self.clauses[i]
+#            if val != '':
+#                print(f"{self.clauses[i]}")
         pass
     
     def addToCurrentTag(self, atag):
@@ -78,22 +98,32 @@ class FilterView(Toplevel):
         match who2:
             case 'entry1':
                 target = self.tag1
+                index = 0
+                focustarget = self.tag2
             case 'entry2':
                 target = self.tag2
+                index = 1
+                focustarget = self.tag3
             case 'entry3':
                 target = self.tag3
+                index = 2
+                focustarget = self.tag4
             case 'entry4':
                 target = self.tag4
+                index = 3
+                focustarget = self.tag1
             case _:
                 return
 
+        # Add the tag to the CURRENT entry
         target.configure(state="normal")
         target.delete('1.0', END)
         # click the button to remove it
         abtn = Button(target, text=f" {atag} ", command=lambda: self.clearItem(target))
         target.window_create("insert", window=abtn, padx=2, pady=2)
         target.configure(state="disabled")
-
+        focustarget.focus() # set focus to the NEXT entry
+        self.clauses[index] = atag
     
     def updateCurrentTags(self):
         self.masterTagList = sorted(self.masterTagList)
@@ -110,12 +140,18 @@ class FilterView(Toplevel):
             self.btnFrame.window_create("insert", window=abtn, padx=2, pady=2)
 
         self.btnFrame.configure(state="disabled")
-                 
+
+def testcall(taglist):
+    for i in range(4):
+        val = taglist[i]
+        if val != '':
+            print(f"{taglist[i]}")
+    
 if __name__ == '__main__': 
 
     root = Tk()
     tags = ['art', 'artoo detoo', 'at-st', 'captain ribman', 'cat staggs', 'celebration v', 'design', 'enlist today', 'film', 'football', 'graphic design', 'hoth', 'illustration', 'japan', 'lego', 'logo', 'mondotees', 'pin-up', 'princess leia', 'propaganda', 'r2-d2', 'raiders', 'rebellion', 'recruitment', 'samurai wars', 'sandpeople', 'sci fi', 'science fiction', 'shan jiang', 'snowtrooper', 'space', 'star wars galaxies', 'stormtrooper', 'tatooine', 'the empire strikes back', 'to hoth and back', 'tusken raiders', 'walker']
-    fwin = FilterView(tags)
+    fwin = FilterView(tags, testcall)
     fwin.mainloop()
 
                  
